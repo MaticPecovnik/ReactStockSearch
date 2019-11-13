a3c1bce9bcc3b523557379ec89a8781bfe892bd9
Matic Pečovnik <maticpeco@hotmail.com>
2019-11-13 21:16:14 +0100

    Some cleaning

--- src/helper.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("node-fetch");
-
-const getDataFromDb = () => {
-  fetch("http://localhost:3000/api/getData")
-    .then(data => data.json())
-    .then(res => console.log(res));
-};
-
-getDataFromDb();
